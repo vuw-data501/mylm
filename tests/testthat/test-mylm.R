3e69d83fd7083047b7bcfe325048c5be37a110b0
RobTomkies <63220353+RobTomkies@users.noreply.github.com>
2022-08-31 16:24:46 +1200

    final tests and error catches

--- tests/testthat/test-mylm.R
+++ tests/testthat/test-mylm.R
@@ -1,24 +1,29 @@
 
-#data
-
-data(iris)
-data(data_tests)
-data_characters <- iris
+#data load
+data_characters <- iris_Rbeta
 data_characters$character_species <- as.character(data_characters$Species)
 data_characters$character_speciesb <- as.character(data_characters$Species)
+
+
+#logrelative error
+LRE <- function(x,c){
+  if(c==0){return(round(-log(abs(x), base = 10)))}
+  else{return(round(-log((abs(x-c)/c) , base = 10)))}
+}
+
 
 
 #Input Checks
 
 test_that("subsetting is possible", {
-  expect_error(mylm(Sepal.Length ~ Sepal.Width, iris, subset = c(1:(nrow(iris)+1))), "Subset stated is larger than the input dataset, please correct")
-  expect_error(mylm(Sepal.Length ~ Sepal.Width, iris, subset = c("hello", "my", "name", "is", "slim")),"Non numeric vector input please correct")
-  expect_error(mylm(Sepal.Length ~ Sepal.Width, iris, subset = list(1,2,3,4)),"Non numeric vector input please correct")
+  expect_error(mylm(Sepal.Length ~ Sepal.Width, iris_Rbeta, subset = c(1:(nrow(iris_Rbeta)+1))), "Subset stated is larger than the input dataset, please correct")
+  expect_error(mylm(Sepal.Length ~ Sepal.Width, iris_Rbeta, subset = c("hello", "my", "name", "is", "slim")),"Non numeric vector input please correct")
+  expect_error(mylm(Sepal.Length ~ Sepal.Width, iris_Rbeta, subset = list(1,2,3,4)),"Non numeric vector input please correct")
 })
 
 test_that("formula only contains names of variables present", {
-  expect_error(mylm(fake.variable ~ Sepal.Length + Petal.Length, iris), "fake.variable are named in formula but not present in data, please check formula")
-  expect_error(mylm(fake.variable ~ fakebeta + Petal.Length, iris), "fake.variable and fakebeta are named in formula but not present in data, please check formula")
+  expect_error(mylm(fake.variable ~ Sepal.Length + Petal.Length, iris_Rbeta), "fake.variable are named in formula but not present in data, please check formula")
+  expect_error(mylm(fake.variable ~ fakebeta + Petal.Length, iris_Rbeta), "fake.variable and fakebeta are named in formula but not present in data, please check formula")
 })
 
 test_that("subsetted data only contains usable data types", {
@@ -41,25 +46,38 @@ test_that("data characteristic warnings and errors work", {
 #Ouput Checks
 
 test_that("returns correct type of values", {
-  expect_type(mylm(Petal.Width ~ Sepal.Length + Petal.Length, iris), "list")
-  expect_length(mylm(Petal.Width ~ Sepal.Length + Petal.Length, iris), 11)
+  expect_type(mylm(Petal.Width ~ Sepal.Length + Petal.Length, iris_Rbeta), "list")
+  expect_length(mylm(Petal.Width ~ Sepal.Length + Petal.Length, iris_Rbeta), 11)
+  expect_type(mylm(Petal.Width ~ Sepal.Length + Petal.Length, iris_Rbeta)$call, "language")
+  expect_type(mylm(Petal.Width ~ Sepal.Length + Petal.Length, iris_Rbeta)$data, "list")
+  expect_type(mylm(Petal.Width ~ Sepal.Length + Petal.Length, iris_Rbeta)$yname, "character")
+  expect_type(mylm(Petal.Width ~ Sepal.Length + Petal.Length, iris_Rbeta)$coef, "double")
+  expect_type(mylm(Petal.Width ~ Sepal.Length + Petal.Length, iris_Rbeta)$sigma, "double")
+  expect_type(mylm(Petal.Width ~ Sepal.Length + Petal.Length, iris_Rbeta)$vcov, "double")
+  expect_type(mylm(Petal.Width ~ Sepal.Length + Petal.Length, iris_Rbeta)$npar, "integer")
+  expect_type(mylm(Petal.Width ~ Sepal.Length + Petal.Length, iris_Rbeta)$df.residual, "integer")
+  expect_type(mylm(Petal.Width ~ Sepal.Length + Petal.Length, iris_Rbeta)$residuals, "double")
+  expect_type(mylm(Petal.Width ~ Sepal.Length + Petal.Length, iris_Rbeta)$fitted.values, "double")
 })
 
 
-x <- mylm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width, iris)
-names(x)
-x$call
-x$formula
-x$data
-x$yname
-x$coef
-x$sigma
-x$vcov
-x$npar
-x$df.residual
-x$residuals
-x$fitted.values
+test_that("check return values matches (structural)", {
+  x <- mylm(Petal.Width ~ Sepal.Length + Petal.Length, iris_Rbeta)
+  expect_equal(length(x$data),  3)
+  expect_equal( length(x$coef),  3)
+  expect_equal( x$npar, 3)
+  expect_equal(length(x$data)^2, 9)
+  expect_equal( length(x$vcov), 9)
+  expect_equal(nrow(x$data) - x$npar, 147)
+  expect_equal(x$df.residual, 147)
+  expect_equal(length(x$residuals), 150)
+  expect_equal(length(x$fitted.values), 150)
+})
 
 
-
+test_that("fitting process working", {
+  x <- mylm(c1 ~ c2, data_tests)
+  expect_true(LRE(x$coef[2], 1)>= 10)
+  expect_true(LRE(x$coef[1], 0)>= 10)
+})
 
